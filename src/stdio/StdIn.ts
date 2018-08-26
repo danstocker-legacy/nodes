62f8d703b39780eb319f6b9b450af8f387d4f160
Dan Stocker <dan@kwaia.com>
2018-08-26 20:44:01 +0100

    Adds exception thrown when trying to feed input to StdIn

--- src/stdio/StdIn.ts
+++ src/stdio/StdIn.ts
@@ -6,6 +6,10 @@ export class StdIn extends Node<null, string | Buffer> {
         process.stdin.on("readable", this.onReadable.bind(this));
     }
 
+    public in(value:null) {
+        throw Error("StdIn is source-only.");
+    }
+
     private onReadable() {
         let chunk = process.stdin.read();
         if (chunk !== null) {
