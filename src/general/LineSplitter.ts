9a2a0c925ab6c2930a161394519cd007e37d4c61
Dan Stocker <dan@kwaia.com>
2018-08-26 22:29:59 +0100

    Updates LineSplitter to emit complete lines only.

--- src/general/LineSplitter.ts
+++ src/general/LineSplitter.ts
@@ -1,9 +1,18 @@
 import {Node} from "../Node";
 
 export class LineSplitter extends Node<string, string> {
+  /**
+   * Line fragment from last input.
+   */
+  private fragment: string = '';
+
   public in(value: string): void {
-    let lines = value.split('\n'),
-      lineCount = lines.length;
+    let text = this.fragment + value,
+      lines = text.split('\n');
+
+    this.fragment = lines.pop();
+
+    let lineCount = lines.length;
     for (let i = 0; i < lineCount; i++) {
       this.out(lines[i]);
     }
