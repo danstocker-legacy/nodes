5a96841a8ccaaff5eaecc5ba57386fea1b239c85
Dan Stocker <dan@kwaia.com>
2018-09-04 12:23:36 +0100

    Removes Synchronizer from src/timing

--- src/timing/index.ts
+++ src/timing/index.ts
@@ -1,5 +1,4 @@
 export {Debouncer} from "./Debouncer";
 export {Delayer} from "./Delayer";
 export {Interval} from "./Interval";
-export {Synchronizer} from "./Synchronizer";
 export {Throttler} from "./Throttler";
