a53c29de1b21fb37de84af1a0308a40a67e10692
Dan Stocker <dan@kwaia.com>
2018-09-06 12:44:51 +0100

    Adds adder-input-tracker example

---
+++ src/examples/adder-input-tracker.ts
@@ -0,0 +1,36 @@
+import {INode, InPort, Inputs, InputTracker, Logger, OutPort} from "..";
+
+class Adder implements INode {
+  public readonly ports: {
+    a: InPort<number>,
+    b: InPort<number>,
+    sum: OutPort<number>
+  };
+  private readonly inputTracker: InputTracker;
+
+  constructor() {
+    this.ports = {
+      a: new InPort(this),
+      b: new InPort(this),
+      sum: new OutPort(this)
+    };
+    this.inputTracker = new InputTracker((inputs: Inputs) => {
+      const sum = (inputs.get(this.ports.a) || 0) + (inputs.get(this.ports.b) || 0);
+      this.ports.sum.send(sum);
+    });
+  }
+
+  public send(inputs: Inputs, tag?: string): void {
+    this.inputTracker.send(inputs, tag);
+  }
+}
+
+const adder: Adder = new Adder();
+const logger: Logger = new Logger();
+
+adder.ports.sum.connect(logger.ports.in);
+
+adder.ports.a.send(1); // 1+0=1
+adder.ports.b.send(1); // 1+1=2
+adder.ports.a.send(2); // 2+1=3
+adder.ports.b.send(2); // 2+2=4
