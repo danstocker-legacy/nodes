5e54314bbdcc606db8b9407a40a6d7ffc57e70a0
Dan Stocker <dan@kwaia.com>
2018-09-07 12:43:22 +0100

    Renames src/stdio to src/io

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ export * from "./abstracts";
 export * from "./functional";
 export * from "./generic";
 export * from "./node";
-export * from "./stdio";
+export * from "./io";
 export * from "./timing";
 export * from "./utils";
