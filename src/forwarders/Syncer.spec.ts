8fdff8f3c622dd9316c23c2e8d0db861d133c3f0
Dan Stocker <dan@kwaia.com>
2018-09-12 14:48:15 +0200

    Adds Syncer w/ spec

---
+++ src/forwarders/Syncer.spec.ts
@@ -0,0 +1,61 @@
+import {InPort, OutPort} from "../node";
+import {Syncer} from "./Syncer";
+
+describe("Syncer", function () {
+  describe("constructor", function () {
+    it("should open ports", function () {
+      const node = new Syncer(3);
+      expect(node.in).toEqual({
+        1: new InPort(node),
+        2: new InPort(node),
+        3: new InPort(node)
+      });
+      expect(node.out).toEqual({
+        $: new OutPort()
+      });
+    });
+
+    describe("on missing argument", function () {
+      it("should default to 2", function () {
+        const node = new Syncer();
+        expect(node.in).toEqual({
+          1: new InPort(node),
+          2: new InPort(node)
+        });
+        expect(node.out).toEqual({
+          $: new OutPort()
+        });
+      });
+    });
+  });
+
+  describe("#send()", function () {
+    let node: Syncer;
+
+    beforeEach(function () {
+      node = new Syncer();
+    });
+
+    describe("when there are no complete input sets", function () {
+      it("should not forward", function () {
+        spyOn(node.out.$, "send");
+        node.send(new Map([[node.in[1] as InPort<number>, 2]]), "2");
+        node.send(new Map([[node.in[2] as InPort<number>, 3]]), "1");
+        expect(node.out.$.send).not.toHaveBeenCalled();
+      });
+    });
+
+    describe("on complete input set", function () {
+      beforeEach(function () {
+        node.send(new Map([[node.in[1] as InPort<number>, 2]]), "2");
+        node.send(new Map([[node.in[2] as InPort<number>, 3]]), "1");
+      });
+
+      it("should forward synced inputs", function () {
+        spyOn(node.out.$, "send");
+        node.send(new Map([[node.in[2] as InPort<number>, 4]]), "2");
+        expect(node.out.$.send).toHaveBeenCalledWith({1: 2, 2: 4}, "2");
+      });
+    });
+  });
+});
---
+++ src/forwarders/Syncer.ts
@@ -0,0 +1,48 @@
+import {SyncerBase} from "../abstracts";
+import {InPort, Inputs, IPort, OutPort, Ports} from "../node";
+
+/**
+ * Synchronizes inputs from multiple ports into a map on a single port.
+ */
+export class Syncer extends SyncerBase {
+  public readonly in: {
+    [key: string]: InPort<any>
+  };
+  public readonly out: {
+    $: OutPort<{ [key: string]: any }>
+  };
+  private readonly lookup: Map<InPort<any>, string>;
+
+  constructor(count: number = 2) {
+    super();
+    this.lookup = new Map();
+    for (let i = 1; i <= count; i++) {
+      this.openInPort(`${i}`, new InPort(this));
+    }
+    this.openOutPort("$", new OutPort());
+  }
+
+  protected onPortOpen<T>(name: string, port: IPort<T>, ports: Ports): void {
+    super.onPortOpen(name, port, ports);
+    if (ports === this.in) {
+      this.lookup.set(port as InPort<T>, name);
+    }
+  }
+
+  protected onPortClose<T>(name: string, port: IPort<T>, ports: Ports): void {
+    super.onPortOpen(name, port, ports);
+    if (ports === this.in) {
+      this.lookup.delete(port as InPort<T>);
+    }
+  }
+
+  protected process(inputs: Inputs, tag: string) {
+    const lookup = this.lookup;
+    const values = {};
+    for (const [port, value] of inputs.entries()) {
+      const name = lookup.get(port);
+      values[name] = value;
+    }
+    this.out.$.send(values, tag);
+  }
+}
