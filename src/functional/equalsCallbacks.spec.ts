d98d2a007c3f7f999427e37d42b15d45df904c06
Dan Stocker <dan@kwaia.com>
2018-10-09 09:23:42 +0100

    Adds property to equalsCallbacks

--- src/functional/equalsCallbacks.spec.ts
+++ src/functional/equalsCallbacks.spec.ts
@@ -1,4 +1,4 @@
-import {reference} from "./equalsCallbacks";
+import {property, reference} from "./equalsCallbacks";
 
 describe("reference", function () {
   describe("when arguments are equal by reference", function () {
@@ -10,9 +10,31 @@ describe("reference", function () {
   });
 
   describe("when arguments are not equal by reference", function () {
-    it("should return true", function () {
+    it("should return false", function () {
       expect(reference(1, 0)).toBe(false);
       expect(reference({}, {})).toBe(false);
+    });
+  });
+});
+
+describe("property", function () {
+  describe("when properties match", function () {
+    it("should return true", function () {
+      expect(property("foo")({foo: 1}, {foo: 1})).toBe(true);
+    });
+  });
+
+  describe("when properties don't match", function () {
+    it("should return false", function () {
+      expect(property("foo")({foo: 2}, {foo: 1})).toBe(false);
+      expect(property("foo")({}, {foo: 1})).toBe(false);
+    });
+  });
+
+  describe("when either argument is undefined", function () {
+    it("should return false", function () {
+      expect(property("foo")(undefined, {foo: 1})).toBeUndefined();
+      expect(property("foo")({foo: 1}, undefined)).toBeUndefined();
     });
   });
 });
--- src/functional/equalsCallbacks.ts
+++ src/functional/equalsCallbacks.ts
@@ -1,8 +1,16 @@
 /**
  * Determines equality by reference.
  */
+import {EqualsCallback} from "./EqualsCallback";
+
 export function reference<T>(a: T, b: T): boolean {
   return a === b;
+}
+
+export function property<T extends { [key: string]: any }>(name: string): EqualsCallback<T> {
+  return (a: T, b: T): boolean => {
+    return a && b && a[name] === b[name];
+  };
 }
 
 // TODO: Add shallow(), deep()
