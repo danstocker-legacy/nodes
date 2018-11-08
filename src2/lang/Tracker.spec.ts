5194f2ec9193afc0113d8cf39ae3c9401c03d4d6
Dan Stocker <dan@kwaia.com>
2018-11-08 12:27:12 +0000

    Removes caching from Tracker

--- src2/lang/Tracker.spec.ts
+++ src2/lang/Tracker.spec.ts
@@ -18,56 +18,10 @@ describe("Tracker", function () {
       node = new Tracker(["foo", "bar"]);
     });
 
-    describe("before first full set", function () {
-      it("should not forward input set", function () {
-        spyOn(node.out.$, "send");
-        node.send(node.in.foo, 1, "1");
-        expect(node.out.$.send).not.toHaveBeenCalled();
-      });
-    });
-
-    describe("on first full set", function () {
-      beforeEach(function () {
-        node.send(node.in.foo, 1, "1");
-      });
-
-      it("should forward full set", function () {
-        const spy = spyOn(node.out.$, "send");
-        node.send(node.in.bar, 10, "1");
-        const args = spy.calls.allArgs();
-        expect(args).toEqual([
-          [{foo: 1, bar: 10}, "1"]
-        ]);
-      });
-
-      describe("when ports have multiple inputs", function () {
-        beforeEach(function () {
-          node.send(node.in.foo, 2, "2");
-        });
-
-        it("should release all full sets", function () {
-          const spy = spyOn(node.out.$, "send");
-          node.send(node.in.bar, 10, "1");
-          const args = spy.calls.allArgs();
-          expect(args).toEqual([
-            [{foo: 1, bar: 10}, "1"],
-            [{foo: 2, bar: 10}, "2"]
-          ]);
-        });
-      });
-    });
-
-    describe("after first full set", function () {
-      beforeEach(function () {
-        node.send(node.in.foo, 1, "1");
-        node.send(node.in.bar, 10, "1");
-      });
-
-      it("should forward all cached inputs", function () {
-        spyOn(node.out.$, "send");
-        node.send(node.in.foo, 2, "2");
-        expect(node.out.$.send).toHaveBeenCalledWith({foo: 2, bar: 10}, "2");
-      });
+    it("should forward all cached inputs", function () {
+      spyOn(node.out.$, "send");
+      node.send(node.in.foo, 2, "2");
+      expect(node.out.$.send).toHaveBeenCalledWith({foo: 2}, "2");
     });
   });
 });
--- src2/lang/Tracker.ts
+++ src2/lang/Tracker.ts
@@ -6,12 +6,11 @@ import {
   TStaticInPorts,
   TStaticOutPorts
 } from "../port";
-import {merge, THash} from "../utils";
+import {THash} from "../utils";
 
 /**
- * Outputs full sets of inputs, where each value corresponds to the latest
- * one received.
- * TODO: Prepare for caching while disconnected.
+ * Outputs sets of inputs, where each value in the set reflects the latest
+ * one received through its corresponding port.
  * @example
  * const tracker: Tracker<{ foo: number, bar: number }>
  * tracker = new Tracker(["foo", "bar"]);
@@ -21,15 +20,10 @@ export class Tracker<T extends THash = THash> extends Node {
   public readonly out: TStaticOutPorts<{
     $: T
   }>;
-
-  private readonly countdown: Set<string | number>;
-  private cache: Array<[T, string]>;
-  private values: T;
+  private readonly values: T;
 
   constructor(fields: Array<string>) {
     super();
-    this.countdown = new Set(fields);
-    this.cache = [];
     this.values = <T> {};
     for (const field of fields) {
       this.addPort(new StaticInPort(field, this));
@@ -38,55 +32,11 @@ export class Tracker<T extends THash = THash> extends Node {
   }
 
   public send<U>(port: IInPort<U>, input: U, tag?: string): void {
-    const countdown = this.countdown;
-    const cache = this.cache;
-    let values = this.values;
     const name = port.name;
-
-    if (countdown.size) {
-      // initializing cache
-      if (!cache.length) {
-        cache.push([values, tag]);
-      }
-
-      // trying to place value in last set of values
-      const current = values[name];
-      if (current !== undefined) {
-        // slot for port is occupied
-        // adding new value set to cache
-        this.values = values = <T> merge({}, values);
-        cache.push([values, tag]);
-      }
-      // assigning new value to port
-      values[name] = input;
-
-      // checking if we've got inputs from all ports
-      countdown.delete(name);
-      if (countdown.size === 0) {
-        // releasing cached inputs and cleaning up
-        this.release();
-        this.cache = undefined;
-      }
-    } else {
+    if (port === this.in[name]) {
+      const values = this.values;
       values[name] = input;
       this.out.$.send(values, tag);
-    }
-  }
-
-  private release(): void {
-    const cache = this.cache;
-    const count = cache.length;
-
-    // populating empty slots in cache
-    for (let i = count - 2; i >= 0; i--) {
-      const current = cache[i][0];
-      const previous = cache[i + 1][0];
-      merge(current, previous);
-    }
-
-    // const port = this.out.$;
-    for (const item of cache) {
-      this.out.$.send(item[0], item[1]);
     }
   }
 }
