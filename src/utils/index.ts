aac99198c4761dea7b995cda30d8dd420fc55791
Dan Stocker <dan@kwaia.com>
2018-11-24 22:08:14 +0000

    Removes IEvent and IError

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,4 @@
 export * from "./utils";
 export {IAny} from "./IAny";
-export {IError} from "./IError";
-export {IEvent} from "./IEvent";
 export {IMuxed} from "./IMuxed";
 export {ValueOf} from "./ValueOf";
