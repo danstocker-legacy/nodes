5eb7ac977d6f1c8783f8862ff6b64735ab7e7c51
Dan Stocker <dan@kwaia.com>
2018-12-02 20:20:33 +0000

    Removes TJsonPath

--- src/utils/TJsonPath.ts
+++
@@ -1,1 +0,0 @@
-export type TJsonPath = Array<string | number>;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,6 +3,5 @@ export {IAny} from "./IAny";
 export {IJsonArray} from "./IJsonArray";
 export {IJsonObject} from "./IJsonObject";
 export {IMuxed} from "./IMuxed";
-export {TJsonPath} from "./TJsonPath";
 export {TJson} from "./TJson";
 export {ValueOf} from "./ValueOf";
