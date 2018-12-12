5247b85b5e04d23a56359c04a1be9d1a74cdcb11
Dan Stocker <dan@kwaia.com>
2018-12-12 21:11:53 +0000

    Adds independent state input to Gate

--- src/nodes/lang/Gate.spec.ts
+++ src/nodes/lang/Gate.spec.ts
@@ -5,7 +5,11 @@ describe("Gate", function () {
     it("should add ports", function () {
       const node = new Gate();
       expect(node.i.mul).toBeDefined();
+      expect(node.i.d_val).toBeDefined();
+      expect(node.i.st_open).toBeDefined();
       expect(node.o.d_val).toBeDefined();
+      expect(node.o.b_mul).toBeDefined();
+      expect(node.o.b_d_val).toBeDefined();
     });
   });
 
@@ -16,19 +20,60 @@ describe("Gate", function () {
       node = new Gate();
     });
 
-    describe("when ref is truthy", function () {
-      it("should forward value", function () {
-        spyOn(node.o.d_val, "send");
-        node.send(node.i.mul, {d_val: 2, st_open: true}, "1");
-        expect(node.o.d_val.send).toHaveBeenCalledWith(2, "1");
+    describe("when sending to `mul`", function () {
+      describe("when ref is truthy", function () {
+        it("should forward value", function () {
+          spyOn(node.o.d_val, "send");
+          node.send(node.i.mul, {d_val: 2, st_open: true}, "1");
+          expect(node.o.d_val.send).toHaveBeenCalledWith(2, "1");
+        });
+      });
+
+      describe("when ref is falsy", function () {
+        it("should not forward value", function () {
+          spyOn(node.o.d_val, "send");
+          node.send(node.i.mul, {d_val: 2, st_open: false}, "1");
+          expect(node.o.d_val.send).not.toHaveBeenCalled();
+        });
+
+        it("should bounce input", function () {
+          spyOn(node.o.b_mul, "send");
+          node.send(node.i.mul, {d_val: 2, st_open: false}, "1");
+          expect(node.o.b_mul.send)
+          .toHaveBeenCalledWith({d_val: 2, st_open: false}, "1");
+        });
       });
     });
 
-    describe("when ref is falsy", function () {
-      it("should not forward value", function () {
-        spyOn(node.o.d_val, "send");
-        node.send(node.i.mul, {d_val: 2, st_open: false}, "1");
-        expect(node.o.d_val.send).not.toHaveBeenCalled();
+    describe("when sending to `d_val`", function () {
+      describe("when gate is open", function () {
+        beforeEach(function () {
+          node.send(node.i.st_open, true, "1");
+        });
+
+        it("should forward value", function () {
+          spyOn(node.o.d_val, "send");
+          node.send(node.i.d_val, 2, "2");
+          expect(node.o.d_val.send).toHaveBeenCalledWith(2, "2");
+        });
+      });
+
+      describe("when gate is closed", function () {
+        beforeEach(function () {
+          node.send(node.i.st_open, false, "1");
+        });
+
+        it("should not forward value", function () {
+          spyOn(node.o.d_val, "send");
+          node.send(node.i.d_val, 2, "2");
+          expect(node.o.d_val.send).not.toHaveBeenCalled();
+        });
+
+        it("should bounce input", function () {
+          spyOn(node.o.b_d_val, "send");
+          node.send(node.i.d_val, 2, "2");
+          expect(node.o.b_d_val.send).toHaveBeenCalledWith(2, "2");
+        });
       });
     });
   });
--- src/nodes/lang/Gate.ts
+++ src/nodes/lang/Gate.ts
@@ -5,7 +5,7 @@ interface IGateInput<V> {
   /** Value to be forwarded. */
   d_val: V;
 
-  /** Whether to forward value. */
+  /** Whether gate is open. */
   st_open: boolean;
 }
 
@@ -17,24 +17,60 @@ interface IGateInputs<V> {
 interface IGateOutputs<V> {
   /** Forwarded value. */
   d_val: V;
+
+  /** Bounced multiple inputs. */
+  b_mul: IGateInput<V>;
+
+  /** Bounced forwarded value. */
+  b_d_val: V;
 }
 
 /**
  * Forwards default input to output when reference input is truthy.
- * TODO: Allow to change st_open independently.
  */
 export class Gate<V> implements ISink, ISource {
-  public readonly i: TInBundle<IGateInputs<V>>;
+  public readonly i: TInBundle<IGateInputs<V> & IGateInput<V>>;
   public readonly o: TOutBundle<IGateOutputs<V>>;
 
+  private open: boolean;
+
   constructor() {
-    MSink.init.call(this, ["mul"]);
-    MSource.init.call(this, ["d_val"]);
+    MSink.init.call(this, ["mul", "d_val", "st_open"]);
+    MSource.init.call(this, ["d_val", "b_mul", "b_d_val"]);
+    this.open = false;
   }
 
-  public send(port: IInPort<IGateInput<V>>, input: IGateInput<V>, tag?: string): void {
-    if (port === this.i.mul && input.st_open) {
-      this.o.d_val.send(input.d_val, tag);
+  public send(
+    port: IInPort<IGateInput<V> | V | boolean>,
+    value: IGateInput<V> | V | boolean,
+    tag?: string
+  ): void {
+    const i = this.i;
+    let open;
+    switch (port) {
+      case i.mul:
+        const mul = value as IGateInput<V>;
+        open = this.open = mul.st_open;
+        if (open) {
+          this.o.d_val.send(mul.d_val, tag);
+        } else {
+          this.o.b_mul.send(mul, tag);
+        }
+        break;
+
+      case i.st_open:
+        open = value as boolean;
+        this.open = open;
+        break;
+
+      case i.d_val:
+        const val = value as V;
+        if (this.open) {
+          this.o.d_val.send(val, tag);
+        } else {
+          this.o.b_d_val.send(val, tag);
+        }
+        break;
     }
   }
 }
