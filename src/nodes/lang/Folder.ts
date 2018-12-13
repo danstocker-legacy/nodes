12dde9baa10f2ae55c78ab3497aedcccf4198d5c
Dan Stocker <dan@kwaia.com>
2018-12-13 06:11:35 +0000

    Adds independent data / event in put in Folder

--- src/nodes/lang/Folder.ts
+++ src/nodes/lang/Folder.ts
@@ -19,6 +19,9 @@ interface IFolderOutputs<I, O> {
   /** Bounced multiple inputs. */
   b_mul: IFolderInput<I>;
 
+  /** Bounced input value. */
+  b_d_val: I;
+
   /** Folded value */
   d_fold: O;
 
@@ -36,14 +39,13 @@ export type TFolderCallback<I, O> = (
  * Emits the next folded value for each input.
  * Takes a callback function which aggregates input values received since the
  * last reset signal.
- * TODO: Add independent data & event ports.
  * @example
  * let sum: Folder<number, number>;
  * sum = new Folder((curr, next) => curr + next, 0);
  * @see {@link https://en.wikipedia.org/wiki/Catamorphism}
  */
 export class Folder<I, O> implements ISink, ISource {
-  public readonly i: TInBundle<IFolderInputs<I>>;
+  public readonly i: TInBundle<IFolderInputs<I> & IFolderInput<I>>;
   public readonly o: TOutBundle<IFolderOutputs<I, O>>;
 
   private readonly cb: TFolderCallback<I, O>;
@@ -51,31 +53,49 @@ export class Folder<I, O> implements ISink, ISource {
   private folded: O;
 
   constructor(cb: TFolderCallback<I, O>, initial?: O) {
-    MSink.init.call(this, ["mul"]);
-    MSource.init.call(this, ["b_mul", "d_fold", "ev_err"]);
+    MSink.init.call(this, ["mul", "d_val", "ev_res"]);
+    MSource.init.call(this, ["b_mul", "b_d_val", "d_fold", "ev_err"]);
     this.cb = cb;
     this.initial = initial;
     this.folded = copy(initial);
   }
 
   public send(
-    port: IInPort<IFolderInput<I>>,
-    value: IFolderInput<I>,
+    port: IInPort<IFolderInput<I> | I | boolean>,
+    value: IFolderInput<I> | I | boolean,
     tag?: string
   ): void {
-    if (port === this.i.mul) {
-      const next = value.d_val;
-      const curr = value.ev_res ?
-        copy(this.initial) :
-        this.folded;
+    const i = this.i;
+    switch (port) {
+      case i.mul:
+        const mul = value as IFolderInput<I>;
+        const curr = mul.ev_res ?
+          copy(this.initial) :
+          this.folded;
 
-      try {
-        const folded = this.folded = this.cb(curr, next, tag);
-        this.o.d_fold.send(folded, tag);
-      } catch (err) {
-        this.o.b_mul.send(value, tag);
-        this.o.ev_err.send(String(err), tag);
-      }
+        try {
+          const folded = this.folded = this.cb(curr, mul.d_val, tag);
+          this.o.d_fold.send(folded, tag);
+        } catch (err) {
+          this.o.b_mul.send(mul, tag);
+          this.o.ev_err.send(String(err), tag);
+        }
+        break;
+
+      case i.d_val:
+        const val = value as I;
+        try {
+          const folded = this.folded = this.cb(this.folded, val, tag);
+          this.o.d_fold.send(folded, tag);
+        } catch (err) {
+          this.o.b_d_val.send(val, tag);
+          this.o.ev_err.send(String(err), tag);
+        }
+        break;
+
+      case i.ev_res:
+        this.folded = copy(this.initial);
+        break;
     }
   }
 }
