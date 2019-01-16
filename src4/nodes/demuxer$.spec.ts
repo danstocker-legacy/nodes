5b5e0876c3e4fe91fa7fbf28b2355bf4a952fb9d
Dan Stocker <dan@kwaia.com>
2019-01-16 20:52:48 +0000

    Adds muxer$, demuxer$

---
+++ src4/nodes/demuxer$.spec.ts
@@ -0,0 +1,19 @@
+import {connect} from "../node";
+import {demuxer$, TDemuxer} from "./demuxer$";
+
+describe("demuxer$", function () {
+  describe("on d_mux", function () {
+    let node: TDemuxer<{ foo: number, bar: number }>;
+
+    beforeEach(function () {
+      node = demuxer$(["foo", "bar"]);
+    });
+
+    it("should emit on specified port", function () {
+      const spy = jasmine.createSpy();
+      connect(node.o.bar, spy);
+      node.i.d_mux({field: "bar", value: 5}, "1");
+      expect(spy).toHaveBeenCalledWith(5, "1");
+    });
+  });
+});
---
+++ src4/nodes/muxer$.spec.ts
@@ -0,0 +1,19 @@
+import {connect} from "../node";
+import {muxer$, TMuxer} from "./muxer$";
+
+describe("muxer$", function () {
+  describe("on input", function () {
+    let node: TMuxer<{ foo: number, bar: number }>;
+
+    beforeEach(function () {
+      node = muxer$(["foo", "bar"]);
+    });
+
+    it("should emit d_mux", function () {
+      const spy = jasmine.createSpy();
+      connect(node.o.d_mux, spy);
+      node.i.bar(5, "1");
+      expect(spy).toHaveBeenCalledWith({field: "bar", value: 5}, "1");
+    });
+  });
+});
