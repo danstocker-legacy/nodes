e2794e3dee14167cb9430bf5c7c90df321fdf203
Dan Stocker <dan@kwaia.com>
2019-01-16 23:33:27 +0000

    Adds merger$

---
+++ src4/nodes/merger$.spec.ts
@@ -0,0 +1,23 @@
+import {connect} from "../node";
+import {merger$, TMerger} from "./merger$";
+
+describe("merger$", function () {
+  describe("on input", function () {
+    let node: TMerger<{ foo: number, bar: number }>;
+
+    beforeEach(function () {
+      node = merger$(["foo", "bar"]);
+    });
+
+    it("should emit all", function () {
+      const spy = jasmine.createSpy();
+      connect(node.o.all, spy);
+      node.i.foo(5, "1");
+      node.i.bar(3, "2");
+      expect(spy.calls.allArgs()).toEqual([
+        [{foo: 5}, "1"],
+        [{foo: 5, bar: 3}, "2"]
+      ]);
+    });
+  });
+});
